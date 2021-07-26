49f0a13d545401efa64fccfe01cdbf027b912242
michael gastal <michaelgastal@gmail.com>
2021-07-26 18:06:01 +0200

    adding routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :informations, only: :show
+  resources :projects, only: :show
+  resources :experiences, only: :show
+  resources :formations, only: :show
+  resources :skills, only: :show
 end
