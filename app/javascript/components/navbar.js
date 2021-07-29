2f987609763c3b3cf3a281d5666172ca29e295d3
michael gastal <michaelgastal@gmail.com>
2021-07-29 19:00:21 +0200

    delete console.log used for tests in navbar.js

--- app/javascript/components/navbar.js
+++ app/javascript/components/navbar.js
@@ -2,8 +2,6 @@ const initAddActiveClassOnClick = () => {
   const navbarLinks = document.querySelectorAll('.navbar-link');
   const currentLocation = location.href;
   navbarLinks.forEach((link) => {
-    console.log(link.href);
-    console.log(currentLocation);
     if(link.href === currentLocation) {
       link.classList.add('navbar-active');
     }
